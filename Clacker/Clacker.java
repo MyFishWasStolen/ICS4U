0a00135172f5390d6311806b6f8e77e0d5992ccc
MyFishWasStolen <mathewsinoj2008@gmail.com>
2026-04-22 14:32:34 +0000

    Wrote and updated comments

--- Clacker/Clacker.java
+++ Clacker/Clacker.java
@@ -1,9 +1,7 @@
 //Programmer: Mathew Sinoj
-//Description: Clacker game - 12 numbered buttons (1-12) are displayed. The player rolls
-//             two dice and may cover the number representing the total or the two
-//             individual dice values. The goal is to cover all 12 numbers in the fewest rolls.
+//Description: Clacker game with GUI
 //Date Created: April 19, 2026
-//Date Revised: April 19, 2026
+//Date Revised: April 22, 2026
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,185 +9,232 @@ import java.awt.event.*;
 import java.util.Random;
 
 public class Clacker
-
 {
 
-	public static void main(String[] args)
+    private Random rand = new Random(); //create random object
+    private boolean[] covered = new boolean[12];  //tracks which numbers have been covered
+    private int rollCount = 0;                     //tracks the number of rolls taken
 
+    private JButton[] numberButtons = new JButton[12];  //the 12 numbered buttons
+    private JLabel die1Label;                            //displays the first die image
+    private JLabel die2Label;                            //displays the second die image
+    private JLabel rollCountLabel;                       //displays the current roll count
+    private JLabel statusLabel;                          //displays game status and win message
+
+    //Constructor: builds and displays the game window
+    //Pre: none
+    //Post: the game window is visible and ready to play
+    public Clacker()
+    {
+
+        JFrame frame = new JFrame("Clacker");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Create a content pane with a vertical BoxLayout and padding
+        JPanel contentPane = new JPanel();
+        contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        contentPane.setBackground(Color.white);
+        contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
+
+        //Create a 2 row by 6 column grid of number buttons labeled 1 to 12
+        JPanel numberPanel = new JPanel();
+        numberPanel.setLayout(new GridLayout(2, 6, 5, 5));
+        numberPanel.setBackground(Color.white);
+        numberPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+
+        for (int i = 0; i < 12; i++)
+        {
+
+            numberButtons[i] = new JButton(String.valueOf(i + 1));
+            numberButtons[i].setActionCommand(String.valueOf(i + 1));
+            numberPanel.add(numberButtons[i]);
+
+        }
+
+        contentPane.add(numberPanel);
+
+        //Create a label that shows the first die image
+        die1Label = new JLabel(new ImageIcon("die1.gif"));
+        die1Label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        die1Label.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
+        contentPane.add(die1Label);
+
+        //Create a label that shows the second die image
+        die2Label = new JLabel(new ImageIcon("die1.gif"));
+        die2Label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        die2Label.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+        contentPane.add(die2Label);
+
+        //Create a label that displays the number of rolls taken
+        rollCountLabel = new JLabel("Rolls: 0");
+        rollCountLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        rollCountLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
+        contentPane.add(rollCountLabel);
+
+        //Create a status label that guides the player and displays the win message
+        statusLabel = new JLabel("Press Roll to start!");
+        statusLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        statusLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
+        contentPane.add(statusLabel);
+
+        //Create a Roll button and attach its listener
+        JButton rollButton = new JButton("Roll");
+        rollButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        rollButton.setActionCommand("Roll");
+        rollButton.addActionListener(new ActionListener()
+        {
+
+            //Handle Roll button click
+            //Pre: none
+            //Post: handleRoll() is called to process the roll
+            public void actionPerformed(ActionEvent event)
+            {
+
+                handleRoll();
+
+            }
+
+        });
+        contentPane.add(rollButton);
+
+        //Create a New Game button and attach its listener
+        JButton newGameButton = new JButton("New Game");
+        newGameButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        newGameButton.setActionCommand("NewGame");
+        newGameButton.addActionListener(new ActionListener()
+        {
+
+            //Handle New Game button click
+            //Pre: none
+            //Post: handleNewGame() is called to reset the game
+            public void actionPerformed(ActionEvent event)
+            {
+
+                handleNewGame();
+
+            }
+
+        });
+        contentPane.add(newGameButton);
+
+        //Add action listeners to each number button
+        for (int i = 0; i < 12; i++)
+        {
+
+            final int num = i + 1;  //must be final to use inside the listener
+
+            numberButtons[i].addActionListener(new ActionListener()
+            {
+
+                //Handle number button click
+                //Pre: none
+                //Post: handleNumberButton() is called with the button's number
+                public void actionPerformed(ActionEvent event)
+                {
+
+                    handleNumberButton(num);
+
+                }
+
+            });
+
+        }
+
+        //Add content pane to frame
+        frame.setContentPane(contentPane);
+
+        //Size and then display the frame
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    //Handle Roll button click
+    //Pre: none
+    //Post: Both dice have been rolled, images updated, and roll count incremented
+    private void handleRoll()
+    {
+
+        int die1Value = rand.nextInt(6) + 1;
+        int die2Value = rand.nextInt(6) + 1;
+        rollCount++;
+
+        die1Label.setIcon(new ImageIcon("die" + die1Value + ".gif"));
+        die2Label.setIcon(new ImageIcon("die" + die2Value + ".gif"));
+        rollCountLabel.setText("Rolls: " + rollCount);
+        statusLabel.setText("Cover " + die1Value + " + " + die2Value + " = " + (die1Value + die2Value));
+
+    }
+
+    //Handle New Game button click
+    //Pre: none
+    //Post: All game state has been reset for a fresh game
+    private void handleNewGame()
+    {
+
+        rollCount = 0;
+        rollCountLabel.setText("Rolls: 0");
+        statusLabel.setText("Press Roll to start!");
+        die1Label.setIcon(new ImageIcon("die1.gif"));
+        die2Label.setIcon(new ImageIcon("die1.gif"));
+
+        for (int i = 0; i < 12; i++)
+        {
+
+            covered[i] = false;
+            numberButtons[i].setText(String.valueOf(i + 1));
+
+        }
+
+    }
+
+    //Handle number button click
+    //Pre: none
+    //Post: The number button has been covered and win condition checked
+    private void handleNumberButton(int num)
+    {
+
+        covered[num - 1] = true;
+        numberButtons[num - 1].setText("");
+
+        //Check if all 12 numbers are covered
+        boolean allCovered = true;
+
+        for (int i = 0; i < 12; i++)
+        {
+
+            if (!covered[i])
+            {
+
+                allCovered = false;
+
+            }
+
+        }
+
+        if (allCovered)
+        {
+
+            statusLabel.setText("You win! Covered in " + rollCount + " rolls!");
+
+        }
+
+    }
+	public static void runGUI()
 	{
-
-		final Random rand = new Random();
-		final boolean[] covered = new boolean[12];
-		final int[] rollCount = {0}; //array used so the value can be changed inside the listener
-
-		//Create and set up the frame
 		JFrame.setDefaultLookAndFeelDecorated(true);
-		JFrame frame = new JFrame("Clacker");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		//Create a content pane with a BoxLayout and empty borders
-		JPanel contentPane = new JPanel();
-		contentPane.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-		contentPane.setBackground(Color.white);
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.PAGE_AXIS));
-
-		//Create a 2 row by 6 column grid of number buttons labeled 1 to 12
-		final JButton[] numberButtons = new JButton[12];
-		JPanel numberPanel = new JPanel();
-		numberPanel.setLayout(new GridLayout(2, 6, 5, 5));
-		numberPanel.setBackground(Color.white);
-		numberPanel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
-
-		for (int i = 0; i < 12; i++)
-		{
-
-			numberButtons[i] = new JButton(String.valueOf(i + 1));
-			numberButtons[i].setActionCommand(String.valueOf(i + 1));
-			numberPanel.add(numberButtons[i]);
-
-		}
-
-		contentPane.add(numberPanel);
-
-		//Create a label that shows the first die image
-		final JLabel die1Label = new JLabel(new ImageIcon("die1.gif"));
-		die1Label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		die1Label.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
-		contentPane.add(die1Label);
-
-		//Create a label that shows the second die image
-		final JLabel die2Label = new JLabel(new ImageIcon("die1.gif"));
-		die2Label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		die2Label.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
-		contentPane.add(die2Label);
-
-		//Create a label that displays the number of rolls taken
-		final JLabel rollCountLabel = new JLabel("Rolls: 0");
-		rollCountLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		rollCountLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 5, 0));
-		contentPane.add(rollCountLabel);
-
-		//Create a status label that guides the player and displays the win message
-		final JLabel statusLabel = new JLabel("Press Roll to start!");
-		statusLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		statusLabel.setBorder(BorderFactory.createEmptyBorder(0, 0, 10, 0));
-		contentPane.add(statusLabel);
-
-		//Create a Roll button
-		JButton rollButton = new JButton("Roll");
-		rollButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		rollButton.setActionCommand("Roll");
-		contentPane.add(rollButton);
-
-		//Create a New Game button
-		JButton newGameButton = new JButton("New Game");
-		newGameButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		newGameButton.setActionCommand("NewGame");
-		contentPane.add(newGameButton);
-
-		//Add action listeners to the Roll button and New Game button
-		rollButton.addActionListener(new ActionListener()
-		{
-
-			//Handle Roll button click
-			//Pre: none
-			//Post: Both dice have been rolled, images updated, and roll count incremented
-			public void actionPerformed(ActionEvent event)
-			{
-
-				int die1Value = rand.nextInt(6) + 1;
-				int die2Value = rand.nextInt(6) + 1;
-				rollCount[0]++;
-
-				die1Label.setIcon(new ImageIcon("die" + die1Value + ".gif"));
-				die2Label.setIcon(new ImageIcon("die" + die2Value + ".gif"));
-				rollCountLabel.setText("Rolls: " + rollCount[0]);
-				statusLabel.setText("Cover " + die1Value + " + " + die2Value + " = " + (die1Value + die2Value));
-
-			}
-
-		});
-
-		newGameButton.addActionListener(new ActionListener()
-		{
-
-			//Handle New Game button click
-			//Pre: none
-			//Post: All game state has been reset for a fresh game
-			public void actionPerformed(ActionEvent event)
-			{
-
-				rollCount[0] = 0;
-				rollCountLabel.setText("Rolls: 0");
-				statusLabel.setText("Press Roll to start!");
-				die1Label.setIcon(new ImageIcon("die1.gif"));
-				die2Label.setIcon(new ImageIcon("die1.gif"));
-
-				for (int i = 0; i < 12; i++)
-				{
-
-					covered[i] = false;
-					numberButtons[i].setText(String.valueOf(i + 1));
-
-				}
-
-			}
-
-		});
-
-		//Add action listeners to each number button
-		for (int i = 0; i < 12; i++)
-		{
-
-			final int num = i + 1; //must be final to use inside the listener
-
-			numberButtons[i].addActionListener(new ActionListener()
-			{
-
-				//Handle number button click
-				//Pre: none
-				//Post: The number button has been covered and win condition checked
-				public void actionPerformed(ActionEvent event)
-				{
-
-					covered[num - 1] = true;
-					numberButtons[num - 1].setText("");
-
-					//check if all 12 numbers are covered
-					boolean allCovered = true;
-
-					for (int i = 0; i < 12; i++)
-					{
-
-						if (!covered[i])
-						{
-
-							allCovered = false;
-
-						}
-
-					}
-
-					if (allCovered)
-					{
-
-						statusLabel.setText("You win! Covered in " + rollCount[0] + " rolls!");
-
-					}
-
-				}
-
-			});
-
-		}
-
-		//Add content pane to frame
-		frame.setContentPane(contentPane);
-
-		//Size and then display the frame
-		frame.pack();
-		frame.setVisible(true);
+		new Clacker();
 
 	}
+
+    //Launch the game
+    //Pre: none
+    //Post: a new Clacker instance is created and the game window is displayed
+    public static void main(String[] args)
+    {
+
+        runGUI();
+
+    }
 
 }
--- PhotoAlbum/PhotoAlbum.java
+++ PhotoAlbum/PhotoAlbum.java
@@ -6,148 +6,164 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A simple Swing-based photo album viewer.
+ * Loads images from a local folder and lets the user cycle through them
+ * using Back and Next buttons.
+ */
 public class PhotoAlbum implements ActionListener
 {
-	JFrame frame;
-	JPanel panel;
-	JLabel label;
-	JButton nextButton;
-	JButton backButton;
+    JFrame frame;
+    JPanel panel;
+    JLabel label;         // Displays the current image
+    JButton nextButton;
+    JButton backButton;
 
-	int currentIndex=0;
+    int currentIndex = 0; // Index of the currently displayed image
 
-	List<String> image = new ArrayList<>();
+    List<String> image = new ArrayList<>(); // File paths of all loaded images
 
-	public PhotoAlbum()
-	{
+    public PhotoAlbum()
+    {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frame = new JFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Main panel with padding around the edges
+        panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        panel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
 
+        // Scan the photoAlbum folder and populate the image list
+        loadImageFromFolder("photoAlbum");
 
-		panel = new JPanel();
-		panel.setLayout(new BorderLayout());
-		panel.setBorder(BorderFactory.createEmptyBorder(20,20,20,20));
+        // Label used to render the image (or an error message if none found)
+        label = new JLabel();
+        label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
+        label.setBorder(BorderFactory.createEmptyBorder(20, 50, 20, 50));
 
-		loadImageFromFolder("photoAlbum");
+        if (image.isEmpty())
+        {
+            // Inform the user if no supported images were found
+            label.setText("No images found. Please add images to the folder 'photoAlbum'.");
+        }
+        else
+        {
+            // Show the first image immediately
+            updateImage();
+        }
+        panel.add(label, BorderLayout.CENTER);
 
-		label = new JLabel();
-		label.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		label.setBorder(BorderFactory.createEmptyBorder(20,50,20,50));
+        // Button panel sits at the bottom of the window
+        JPanel buttonPanel = new JPanel();
 
-		if (image.isEmpty())
-		{
+        backButton = new JButton("Back");
+        backButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        backButton.setActionCommand("back");
+        backButton.addActionListener(this);
+        buttonPanel.add(backButton);
 
-			label.setText("No images found. Please add images to the folder 'photoAlbum'.");
+        nextButton = new JButton("Next");
+        nextButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
+        nextButton.setActionCommand("next");
+        nextButton.addActionListener(this);
+        buttonPanel.add(nextButton);
 
-		}
-		else
-		{
+        panel.add(buttonPanel, BorderLayout.SOUTH);
 
-			updateImage();
+        frame.setContentPane(panel);
+        frame.setSize(800, 600);
+        frame.setVisible(true);
+    }
 
-		}
-		panel.add(label, BorderLayout.CENTER);
+    /**
+     * Handles Back and Next button clicks.
+     * Wraps around at both ends of the image list using modular arithmetic.
+     */
+    @Override
+    public void actionPerformed(ActionEvent event)
+    {
+        String eventName = event.getActionCommand();
 
+        // Nothing to navigate if the folder was empty
+        if (image.isEmpty()) return;
 
-		JPanel buttonPanel = new JPanel();
+        if (eventName.equals("next"))
+        {
+            // Advance forward; wrap to 0 after the last image
+            currentIndex = (currentIndex + 1) % image.size();
+        }
+        else if (eventName.equals("back"))
+        {
+            // Step backward; wrap to the last image from index 0
+            currentIndex = (currentIndex - 1 + image.size()) % image.size();
+        }
 
-		backButton = new JButton("Back");
-		backButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		backButton.setActionCommand("back");
-		backButton.addActionListener(this);
-		buttonPanel.add(backButton);
+        updateImage(); // Refresh the label with the new image
+    }
 
-		nextButton = new JButton("Next");
-		nextButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		nextButton.setActionCommand("next");
-		nextButton.addActionListener(this);
-		buttonPanel.add(nextButton);
+    /**
+     * Scans the given folder path for JPG and PNG images and stores their
+     * absolute paths in the image list. Creates the folder if it doesn't exist.
+     *
+     * @param path relative or absolute path to the image folder
+     */
+    private void loadImageFromFolder(String path)
+    {
+        File file = new File(path);
 
+        // Create the directory if it is missing so the app doesn't crash
+        if (!file.exists())
+        {
+            file.mkdir();
+        }
 
-		panel.add(buttonPanel, BorderLayout.SOUTH);
+        File[] files = file.listFiles();
+        if (files != null)
+        {
+            for (File f : files)
+            {
+                String name = f.getName().toLowerCase();
 
+                // Only include common image formats
+                if (name.endsWith(".jpg") || name.endsWith(".jpeg") || name.endsWith(".png"))
+                {
+                    image.add(f.getAbsolutePath());
+                }
+            }
+        }
+    }
 
-		frame.setContentPane(panel);
+    /**
+     * Loads the image at currentIndex, scales it to fit the window,
+     * and sets it on the label.
+     */
+    private void updateImage()
+    {
+        ImageIcon icon = new ImageIcon(image.get(currentIndex));
 
-		frame.setSize(800,600);
-		frame.setVisible(true);
+        // Scale the image to fill most of the window while keeping it smooth
+        Image scaledImage = icon.getImage().getScaledInstance(
+            frame.getWidth() - 100,
+            frame.getHeight() - 150,
+            Image.SCALE_SMOOTH
+        );
 
-	}
-	@Override
-	public void actionPerformed(ActionEvent event)
-	{
+        label.setIcon(new ImageIcon(scaledImage));
+    }
 
-		String eventName = event.getActionCommand();
+    /**
+     * Applies the system look-and-feel decoration and launches the album.
+     * Called on the Event Dispatch Thread via SwingUtilities.
+     */
+    private static void runGUI()
+    {
+        JFrame.setDefaultLookAndFeelDecorated(true);
+        new PhotoAlbum();
+    }
 
-		if(image.isEmpty()) return;
-
-		if(eventName.equals("next"))
-		{
-			currentIndex = (currentIndex + 1) % image.size();
-		}
-		else if (eventName.equals("back"))
-		{
-
-			currentIndex = (currentIndex - 1 + image.size()) % image.size();
-
-		}
-
-		updateImage();//update image
-
-
-
-	}
-
-	private void loadImageFromFolder (String path)
-	{
-		File file = new File(path);
-		if (!file.exists())
-		{
-			file.mkdir();
-		}
-
-		File[] files = file.listFiles();
-		if (files != null)
-		{
-			for (File f : files)
-			{
-				String name = f.getName().toLowerCase();
-				if(name.endsWith(".jpg") || name.endsWith(".jpeg") || name.endsWith(".png"))
-				{
-					image.add(f.getAbsolutePath());
-				}
-			}
-
-		}
-
-
-	}
-
-
-	private void updateImage()
-	{
-		ImageIcon icon = new ImageIcon(image.get(currentIndex));
-
-		Image image = icon.getImage().getScaledInstance(frame.getWidth()-100, frame.getHeight()-150, Image.SCALE_SMOOTH);
-
-		label.setIcon(new ImageIcon(image));
-
-
-	}
-
-	private static void runGUI()
-	{
-
-		JFrame.setDefaultLookAndFeelDecorated(true);
-		new PhotoAlbum();
-
-
-	}
-	public static void main(String[] args)
-	{
-		runGUI();
-
-
-	}
+    public static void main(String[] args)
+    {
+        // Swing UIs must be created on the Event Dispatch Thread
+        SwingUtilities.invokeLater(PhotoAlbum::runGUI);
+    }
 }
